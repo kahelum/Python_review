2ffb50e1d25e3ebbd9a7242f17dfbc6ce42709f2
Hyeonmin <kahelum@gmail.com>
2019-12-06 02:56:30 +0900

    commit

---
+++ Python_숫자자료형/Python_숫자자료형/Python_4_조건문.py
@@ -0,0 +1,40 @@
+score = 75
+if score >= 80:
+    print("Good")
+    print("점수가 80점 이상입니다.")
+elif score >= 70:
+    print("Not Bad ")
+    print("점수가 70점 이상입니다.")
+else:
+    print("Bad")
+print("\nEtc\n")
+
+if score < 90 and score >= 70:      # and 또는 or
+    print ("Not Bad")
+
+print()
+list = [1, 2, 3]
+if 2 in list:
+    print("2는 리스트에 포함되어 있습니다.")
+
+
+"""
+score = 75
+if score >= 80:
+    print("Good")
+    print("The score is 80 or higher.")
+elif score >= 70:
+    print("Not Bad ")
+    print("The score is 70 or higher.")
+else:
+    print("Bad")
+print("\nEtc\n")
+
+if score < 90 and score >= 70:      # and 또는 or
+    print ("Not Bad")
+
+print()
+list = [1, 2, 3]
+if 2 in list:
+    print("2 is included in the list.")
+"""
